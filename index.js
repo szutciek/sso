08e42b1601a150d4295cc8da3d3b6f6afdd1eed8
szutciek <szutermaciek@gmail.com>
2025-02-14 18:12:49 +0100

    Fixed Config

--- index.js
+++ index.js
@@ -5,20 +5,16 @@ import "./database.js";
 import apiRouter from "./api/index.js";
 import safeErrorHandler from "./utils/safeErrorHandler.js";
 
-import { server } from "./config.js";
+import { dirname, server } from "./config.js";
 
-app.get("/", (_, res) =>
-  res.sendFile(import.meta.dirname + "/client/dist/index.html")
-);
+app.get("/", (_, res) => res.sendFile(dirname + "/client/dist/index.html"));
 
 app.use("/api", apiRouter);
 
 app.use(express.static("client/dist/"));
 app.use(express.static("public/"));
 
-app.get("/*", (_, res) =>
-  res.sendFile(import.meta.dirname + "/client/dist/index.html")
-);
+app.get("/*", (_, res) => res.sendFile(dirname + "/client/dist/index.html"));
 
 app.use(safeErrorHandler);
 
